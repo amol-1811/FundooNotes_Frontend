bdfcfa03f1317e8e0095d2239c797ed888c57dbe
Amol Gosavi <gosaviamol1811@gmail.com>
2025-04-19 12:49:44 +0530

    integration for login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/services/user/user.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-login',
@@ -11,7 +14,11 @@ export class LoginComponent implements OnInit{
   hidePassword = true;
   selectedLanguage = 'en-US';
 
-  constructor(private fb:  FormBuilder) {}
+  constructor(private fb:  FormBuilder,
+              private userSer: UserService, 
+              private router: Router, 
+              private snackBar: MatSnackBar
+  ) {}
 
   ngOnInit(): void {
     this.loginForm = this.fb.group({
@@ -24,6 +31,32 @@ export class LoginComponent implements OnInit{
     if(this.loginForm.valid) {
       const loginData = this.loginForm.value;
       console.log('Login Data:', loginData);
+      
+      this.userSer.login(loginData).subscribe(
+        (response) => {
+          console.log('Login successful:', response);
+          this.snackBar.open('Login successful!', 'Close', {
+            duration: 2000,
+            horizontalPosition: 'center',
+            verticalPosition: 'top'
+          });
+        },
+        (error) => {
+          console.error('Login failed:', error);
+          this.snackBar.open('Login failed. Please try again.', 'Close', {
+            duration: 2000,
+            horizontalPosition: 'center',
+            verticalPosition: 'top',
+          });
+        }
+      );
+    }
+    else {
+      this.snackBar.open('Please fill in all required fields.', 'Close', {
+        duration: 3000,
+        horizontalPosition: 'center',
+        verticalPosition: 'top'
+      });
     }
   }
 }
--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -2,13 +2,16 @@ import { Injectable } from '@angular/core';
 import { HttpService } from '../http/http.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
+  constructor(private http: HttpService) {}
 
-  constructor(private http: HttpService) { }
+  register(payload: any) {
+    return this.http.postApi('/register', payload);
+  }
 
-  register(payload: any){
-    return this.http.postApi("/register", payload)
+  login(payload: any) {
+    return this.http.postApi('/login', payload);
   }
 }
